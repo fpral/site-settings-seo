7761cd995c6a3bf270346185a9c979623ffc5b99
Thomas Draier <tdraier@jahia.com>
2018-01-19 10:18:04 +0100

    Fixed layout / theme

--- src/javascript/components/SeoSiteSettings.jsx
+++ src/javascript/components/SeoSiteSettings.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
-import {TableHeader, TableHeaderColumn, TableRowColumn} from 'material-ui';
+import {AppBar, MuiThemeProvider, Paper, Reboot, Toolbar, Typography, withStyles} from 'material-ui';
 import {store, theme} from '@jahia/react-dxcomponents';
 import {client} from '@jahia/apollo-dx';
 import {VanityUrlsTable} from "./VanityUrlsTable";
 import {ApolloProvider} from 'react-apollo';
 import {Provider} from 'react-redux'
 
+const styles = theme => ({
+    root: theme.mixins.gutters({
+        padding: 16,
+        margin: theme.spacing.unit * 3,
+    }),
+});
+
 let SeoSiteSettings = function (props) {
 
     return (
-        <Provider store={store}>
-            <ApolloProvider client={client}>
-                <div>
-                    <div className="page-header">
-                        <h2>SEO</h2>
-                    </div>
+        <MuiThemeProvider theme={theme}>
+            <Provider store={store}>
+                <ApolloProvider client={client}>
+                    <div>
+                        <AppBar position="static" >
+                            <Toolbar>
+                                <Typography type="title" color="inherit">
+                                    SEO
+                                </Typography>
+                            </Toolbar>
+                        </AppBar>
 
-                    <div className="panel panel-default">
-                        <VanityUrlsTable></VanityUrlsTable>
+                        <Paper elevation={1} className={props.classes.root}>
+                            <VanityUrlsTable></VanityUrlsTable>
+                        </Paper>
                     </div>
-                </div>
-            </ApolloProvider>
-        </Provider>
+                </ApolloProvider>
+            </Provider>
+        </MuiThemeProvider>
     )
 };
+
+SeoSiteSettings = withStyles(styles)(SeoSiteSettings);
 
 export {SeoSiteSettings};
