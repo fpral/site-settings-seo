fe3c169114bfeb0e1f6141ecdf0d570af28a16af
David Griffon <dgriffon@jahia.com>
2018-02-19 17:32:35 +0100

    BACKLOG-7686 : remove redux from the pagination

--- src/javascript/components/VanityUrlTable.jsx
+++ src/javascript/components/VanityUrlTable.jsx
@@ -1,65 +1,23 @@
 import React from 'react';
-import {connect, Provider} from 'react-redux'
 import {VanityUrlTableData} from './VanityUrlTableData'
-import {reducers, store, resetStateReducer} from "@jahia/react-dxcomponents";
 
 class VanityUrlTable extends React.Component {
     constructor(props) {
         super(props);
-        this.Component = connect(this.mapStateToProps, this.mapDispatchToProps)(VanityUrlTableData);
+        this.state = {currentPage:0, pageSize:5};
+        this.onChangePage.bind(this);
+        this.onChangeRowsPerPage.bind(this);
     }
 
-    mapStateToProps(state, ownProps) {
-        return {
-            pageSize: state["pageSize"] ? state["pageSize"] : 5,
-            currentPage: state["currentPage"] ? state["currentPage"] : 0,
-            path: ownProps.path
-        }
-    };
-
-    mapDispatchToProps(dispatch, ownProps) {
-        return {
-            onChangePage(page) {
-                dispatch({
-                    type: 'SELECT_PAGE',
-                    page: page
-                })
-            },
-            onChangeRowsPerPage(pageSize) {
-                dispatch({
-                    type: 'CHANGE_PAGE_SIZE',
-                    pageSize: pageSize
-                })
-            },
-
-        }
-    };
-
-    componentWillMount() {
-        reducers["currentPage"] = (state = 0, action) => {
-            if (action.type === 'SELECT_PAGE') {
-                return action.page
-            }
-            return state;
-        };
-        reducers["pageSize"] = (state = 0, action) => {
-            if (action.type === 'CHANGE_PAGE_SIZE') {
-                return action.pageSize
-            }
-            return state;
-        };
+    onChangePage = (newPage) => {
+        this.setState({currentPage: newPage});
     }
-
-    componentWillUnmount() {
-        reducers["currentPage"] = resetStateReducer;
-        reducers["pageSize"] = resetStateReducer;
-        store.dispatch({type:"RESET_STATE"});
-        delete reducers["currentPage"];
+    onChangeRowsPerPage = (newRowsPerPage) => {
+        this.setState({pageSize: newRowsPerPage})
     }
 
     render() {
-        let Component = this.Component;
-        return (<Provider store={ store }><Component {...this.props}/></Provider>)
+        return (<VanityUrlTableData {...this.props} {...this.state} onChangePage={this.onChangePage} onChangeRowsPerPage={this.onChangeRowsPerPage}/>)
     }
 }
 
