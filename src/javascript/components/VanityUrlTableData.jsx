e1a3c93b2615f0e2aa22c40e85c825d917b8073d
David Griffon <dgriffon@jahia.com>
2018-02-26 13:28:38 +0100

    BACKLOG-7705 : fix issue when no live url exists at all

--- src/javascript/components/VanityUrlTableData.jsx
+++ src/javascript/components/VanityUrlTableData.jsx
@@ -19,8 +19,8 @@ let mapResultsToProps = ({data, ownProps}) => {
 
             rows: _.map(data.jcr.nodesByQuery.nodes, contentNode => {
 
-                let defaultUrls = _.keyBy(_.map(contentNode.vanityUrls, vanityUrlNode => ({uuid: vanityUrlNode.uuid, default: vanityUrlNode})), 'uuid');
-                let liveUrls = _.keyBy(_.map(contentNode.liveNode.vanityUrls, vanityUrlNode => ({uuid:vanityUrlNode.uuid, live: vanityUrlNode})), 'uuid');
+                let defaultUrls = contentNode.vanityUrls ? _.keyBy(_.map(contentNode.vanityUrls, vanityUrlNode => ({uuid: vanityUrlNode.uuid, default: vanityUrlNode})), 'uuid') : {};
+                let liveUrls = contentNode.liveNode ? _.keyBy(_.map(contentNode.liveNode.vanityUrls, vanityUrlNode => ({uuid:vanityUrlNode.uuid, live: vanityUrlNode})), 'uuid') : {};
                 let urlPairs = _.merge(defaultUrls, liveUrls);
                 urlPairs = _.sortBy(urlPairs, urlPair => (urlPair.default ? urlPair.default.language : urlPair.live.language));
 
