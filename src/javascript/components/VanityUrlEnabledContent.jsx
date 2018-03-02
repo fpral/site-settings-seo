d8040641d92d6ce2f8dec32c11eee141da1b80c6
oyevstafyev <oievstafiev@jahia.com>
2018-03-02 11:54:31 +0100

    [BACKLOG-7707] Show all: draft implementation

--- src/javascript/components/VanityUrlEnabledContent.jsx
+++ src/javascript/components/VanityUrlEnabledContent.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {translate} from 'react-i18next';
+import {graphql} from 'react-apollo';
+import gql from "graphql-tag";
+import * as _ from "lodash";
 
-import { VanityUrlListDefault, VanityUrlListLive, } from './VanityUrlList';
+import { VanityUrlListDefault, VanityUrlListLive } from './VanityUrlList';
 
 import {
+    Button,
     Collapse,
     Grid,
     ListItem,
@@ -11,6 +16,11 @@ import {
     Paper,
     withStyles
 } from 'material-ui';
+
+import {
+    ExpandLess,
+    ExpandMore
+} from 'material-ui-icons';
 
 const styles = theme => ({
     root: {
@@ -22,40 +32,136 @@ const styles = theme => ({
     },
 });
 
-import {
-    ExpandLess,
-    ExpandMore
-} from 'material-ui-icons';
-
 class VanityUrlEnabledContent extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            open: false
+            expanded: false,
+            localFilteringEnabled: true
         };
     }
 
-    handleItemClick = () => {
-        this.setState({open: !this.state.open});
+    handleExpandCollapseClick = () => {
+        this.setState({
+            expanded: !this.state.expanded
+        });
+    };
+
+    propsToOptions = (props) => {
+        return {
+            variables: {
+                uuid: this.props.content.uuid
+            },
+            fetchPolicy: 'network-only'
+        }
+    }
+
+    resultsToProps = ({data, ownProps}) => {
+
+        if (!data.jcr) {
+            return {
+                ...ownProps,
+                vanityUrls: [],
+                filterText: this.props.filterText
+            };
+        }
+
+        let contentNode = data.jcr.nodeById;
+        let defaultUrls = _.keyBy(_.map(contentNode.vanityUrls, vanityUrlNode => ({uuid: vanityUrlNode.uuid, default: vanityUrlNode})), 'uuid');
+        let liveUrls = contentNode.liveNode ? _.keyBy(_.map(contentNode.liveNode.vanityUrls, vanityUrlNode => ({uuid:vanityUrlNode.uuid, live: vanityUrlNode})), 'uuid') : {};
+        let urlPairs = _.merge(defaultUrls, liveUrls);
+        urlPairs = _.sortBy(urlPairs, urlPair => (urlPair.default ? urlPair.default.language : urlPair.live.language));
+
+        return {
+            ...ownProps,
+            vanityUrls: _.values(urlPairs),
+            filterText: this.props.filterText
+        };
+    }
+
+    getLocalFilteringDisabledVanityUrlList(VanityUrlList) {
+
+        let query = gql`
+            query NodesQuery($uuid: String!) {
+                jcr {
+                    nodeById(uuid: $uuid) {
+                        vanityUrls {
+                            active
+                            default
+                            url
+                            language
+                            uuid
+                            path
+                        }
+                        liveNode: nodeInWorkspace(workspace: LIVE) {
+                            vanityUrls {
+                                active
+                                default
+                                url
+                                language
+                                uuid
+                                path
+                            }
+                        }
+                    }
+                }
+            }
+        `;
+
+        VanityUrlList = graphql(query, {
+            options: this.propsToOptions,
+            props: this.resultsToProps
+        })(VanityUrlList);
+
+        return <VanityUrlList/>
+    }
+
+    handleFilterSwitchClick = (e) => {
+        e.stopPropagation();
+        this.setState({
+            localFilteringEnabled: !this.state.localFilteringEnabled
+        });
     };
 
     render() {
-        const {content, filterText, classes} = this.props;
+
+        const { content, filterText, classes, t } = this.props;
+
+        let filterSwitchButtonLabel = null;
+        if (filterText && this.state.expanded) {
+            if (this.state.localFilteringEnabled) {
+                filterSwitchButtonLabel = t('label.localFilter.switchOff');
+            } else {
+                filterSwitchButtonLabel = t('label.localFilter.switchOn');
+            }
+        }
+
+        let vanityUrlListDefault = null;
+        let vanityUrlListLive = null;
+        if (this.state.localFilteringEnabled) {
+            vanityUrlListDefault = <VanityUrlListDefault vanityUrls={content.urls} filterText={filterText}/>;
+            vanityUrlListLive = <VanityUrlListLive vanityUrls={content.urls} filterText={filterText}/>;
+        } else {
+            vanityUrlListDefault = this.getLocalFilteringDisabledVanityUrlList(VanityUrlListDefault);
+            vanityUrlListLive = this.getLocalFilteringDisabledVanityUrlList(VanityUrlListLive);
+        }
+
         return (
             <div className={this.props.classes.root}>
                 <Paper elevation={1}>
-                    <ListItem button onClick={() => this.handleItemClick()} >
-                        <ListItemIcon>{this.state.open ? <ExpandLess/> : <ExpandMore/>}</ListItemIcon>
+                    <ListItem onClick={() => this.handleExpandCollapseClick()} >
+                        <ListItemIcon>{this.state.expanded ? <ExpandLess/> : <ExpandMore/>}</ListItemIcon>
                         <ListItemText inset primary={content.displayName} secondary={content.path}/>
+                        {filterSwitchButtonLabel ? <Button onClick={(e) => this.handleFilterSwitchClick(e)}>{filterSwitchButtonLabel}</Button> : ''}
                     </ListItem>
-                    <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                    <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
                         <Grid container spacing={24}  className={classes.nested}>
                             <Grid item xs={12} lg={6}>
-                                <VanityUrlListDefault vanityUrls={content.urls} filterText={filterText}/>
+                                {vanityUrlListDefault}
                             </Grid>
                             <Grid item xs={12} lg={6}>
-                                <VanityUrlListLive vanityUrls={content.urls} filterText={filterText}/>
+                                {vanityUrlListLive}
                             </Grid>
                         </Grid>
                     </Collapse>
@@ -65,6 +171,6 @@ class VanityUrlEnabledContent extends React.Component {
     }
 }
 
-VanityUrlEnabledContent = withStyles(styles)(VanityUrlEnabledContent);
+VanityUrlEnabledContent = withStyles(styles)(translate('site-settings-seo')(VanityUrlEnabledContent));
 
 export {VanityUrlEnabledContent};
