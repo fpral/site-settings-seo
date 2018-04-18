7a995175eaadddbc44e3d465cd14f0ab968ccf13
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-04-18 16:38:10 +0200

    [BACKLOG-7803] Extended the "Delete Default Mappings" confirmation window with a "safety" checkbox and information about vanities, which will be deleted

--- src/javascript/components/Deletion.jsx
+++ src/javascript/components/Deletion.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import * as _ from "lodash";
-import {Button} from 'material-ui';
+import {Button, Checkbox, FormControlLabel} from 'material-ui';
 import {compose, graphql} from "react-apollo/index";
 import {translate} from "react-i18next";
 import Dialog, {DialogActions, DialogContent, DialogContentText, DialogTitle} from 'material-ui/Dialog';
+import {Table, TableBody, TableRow, TableCell} from 'material-ui';
 import {query} from './VanityUrlTableData';
 import {withNotifications} from '@jahia/react-dxcomponents';
 import {withVanityMutationContext} from "./VanityMutationsProvider";
@@ -20,27 +21,68 @@ class Deletion extends React.Component {
             props.onClose();
             notificationContext.notify(t('label.deletionConfirmed'));
         };
+        this.state = {
+            deleteDisabled: true,
+        };
+       this.handleDeleteDisabled = this.handleDeleteDisabled.bind(this);
+       this.handleClose = this.handleClose.bind(this);
     }
 
-    render() {
-        const { open, onClose, t } = this.props;
+    handleDeleteDisabled() {
+        this.setState((previous) => ({deleteDisabled: !previous.deleteDisabled}));
+    }
+
+    handleClose() {
+        this.setState({
+            deleteDisabled: true
+        });
+        this.props.onClose()
+    }
+
+	render() {
+        const { open, classes, onClose, t } = this.props;
         return (
             <div>
                 <Dialog open={open} fullWidth={true} onClose={onClose} aria-labelledby="alert-dialog-title"
                         aria-describedby="alert-dialog-description">
                     <DialogTitle id="alert-dialog-title">{t('label.dialogs.delete.title')}</DialogTitle>
                     <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            {t('label.dialogs.delete.content')}
+                        <DialogContentText id="alert-dialog-headline">
+                            {t('label.dialogs.delete.headline')}
+                        </DialogContentText><br/>
+                        <DialogContentText id="alert-dialog-content">
+                          	{t('label.dialogs.delete.content')}
                         </DialogContentText>
                     </DialogContent>
+                    <DialogContent>
+                        <Table>
+                            <TableBody>
+                                {this.props.urlPairs.map((url, i) =>
+                                    <TableRow key={i}>
+                                        <TableCell>{url.default.targetNode.path}</TableCell>
+                                        <TableCell>{url.default.url}</TableCell>
+                                        <TableCell>{url.default.language}</TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </DialogContent>
                     <DialogActions>
-                        <Button onClick={onClose} color="primary">
-                            {t('label.dialogs.delete.cancel')}
-                        </Button>
-                        <Button onClick={() => {this.delete()}} color="primary" autoFocus>
-                            {t('label.dialogs.delete.delete')}
-                        </Button>
+                        <span>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox checked={!this.state.deleteDisabled}
+                                              onChange={() => this.handleDeleteDisabled()} />
+                                }
+                                label={t("label.dialogs.delete.terms")}
+                            />
+	                        <Button onClick={this.handleClose} color="primary">
+	                            {t('label.dialogs.delete.cancel')}
+	                        </Button>
+	                        <Button onClick={() => {this.delete()}} color="primary" autoFocus disabled={this.state.deleteDisabled}>
+	                            {t('label.dialogs.delete.delete')}
+	                        </Button>
+                        </span>
                     </DialogActions>
                 </Dialog>
             </div>
