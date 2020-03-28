5c94333581dac94a36839b231cf4c405efb298fb
Francois Pral <fpral@jahia.com>
2020-03-28 16:38:32 +0100

    BACKLOG-12942: updated position in the additional accordion

--- src/javascript/register.js
+++ src/javascript/register.js
@@ -2,7 +2,7 @@
     window.jahia.i18n.loadNamespaces('site-settings-seo');
 
     window.jahia.uiExtender.registry.add('adminRoute', 'site-settings-seo', {
-        targets: ['jcontent:30'],
+        targets: ['jcontent:50'],
         icon: null,
         label: 'site-settings-seo:label.title',
         isSelectable: true,
