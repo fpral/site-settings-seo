63cab7b3b0d4858f3dcc5833ca91b88b0f81fe5f
Francois Pral <fpral@jahia.com>
2020-03-28 16:56:44 +0100

    fixed priority

--- src/javascript/register.js
+++ src/javascript/register.js
@@ -2,7 +2,7 @@
     window.jahia.i18n.loadNamespaces('site-settings-seo');
 
     window.jahia.uiExtender.registry.add('adminRoute', 'site-settings-seo', {
-        targets: ['jcontent:50'],
+        targets: ['jcontent:60'],
         icon: null,
         label: 'site-settings-seo:label.title',
         isSelectable: true,
